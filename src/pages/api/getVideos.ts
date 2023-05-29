462da9339379a21ec9ab2dd5a8940b80421234fe
bluvar <0x317374@gmail.com>
2023-05-29 18:41:52 +0700

    chore(checkpoint): 29/05/2023, 11:41:52 GMT+0000

--- src/pages/api/getVideos.ts
+++ src/pages/api/getVideos.ts
@@ -1,5 +1,50 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import * as fs from "fs";
+import path from "path";
+import * as os from "os";
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  res.status(200).json({ name: "John Doe" });
+  let videoDir = path.join(os.homedir(), "OneDrive", "Documents", "Zoom");
+  if (!fs.existsSync(videoDir)) {
+    videoDir = path.join(os.homedir(), "Documents", "Zoom");
+  }
+
+  const videoFiles = getAllFilesRecursive(videoDir)
+    .filter((v) => v.endsWith(".mp4"))
+    .map((v) => {
+      const dirName = path.dirname(v).split(path.sep).pop();
+      const dateInfo = dirName!.split(" ");
+      const date = dateInfo.shift() as string;
+      const time = (dateInfo.shift() as string).split(".").join(":");
+      const keywords = dateInfo.join(" ");
+
+      return {
+        fullPath: v,
+        fileName: path.basename(v),
+        dirName,
+        date,
+        time,
+        keywords,
+        saveAt: new Date(`${date} ${time}`),
+      };
+    });
+
+  res.status(200).json({
+    files: videoFiles,
+  });
+}
+
+function getAllFilesRecursive(dirPath: string, arrayOfFiles: string[] = []): string[] {
+  const files = fs.readdirSync(dirPath);
+
+  files.forEach((file) => {
+    const filePath = path.join(dirPath, file);
+    if (fs.statSync(filePath).isDirectory()) {
+      arrayOfFiles = getAllFilesRecursive(filePath, arrayOfFiles);
+    } else {
+      arrayOfFiles.push(filePath);
+    }
+  });
+
+  return arrayOfFiles;
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,30 @@
+import { Button, Divider, TextInput } from "@mantine/core";
+import { useLocalStorage } from "react-use";
+
 export default function Home() {
-  return <main>Hello</main>;
+  const [folderId, setFolderId] = useLocalStorage(":folderId", "");
+  const [zoomFolder, setZoomFolder] = useLocalStorage(":zoomFolder", "Documents/Zoom");
+
+  return (
+    <main className="m-auto max-w-xl">
+      <div className="p-4 flex flex-col gap-2">
+        <TextInput
+          label="Google Drive Folder ID"
+          value={folderId}
+          onChange={(e) => setFolderId(e.target.value)}
+          placeholder={"E.g: 1Gz5K3YtRNK6HZITIZ8rk-knuJpwD-uMv"}
+        />
+        <TextInput
+          label="Folder containing Zoom's video recordings"
+          value={zoomFolder}
+          onChange={(e) => setZoomFolder(e.target.value)}
+          placeholder={"E.g: Documents/Zoom"}
+        />
+        <Divider />
+        <div>
+          <Button variant="gradient">Reload Files</Button>
+        </div>
+      </div>
+    </main>
+  );
 }
