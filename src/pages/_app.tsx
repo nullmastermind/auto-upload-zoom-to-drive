964be3dbff3692c75455ac026a82ec4a20f63742
bluvar <0x317374@gmail.com>
2023-05-30 23:55:44 +0700

    chore(checkpoint): 30/05/2023, 16:55:44 GMT+0000

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,12 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { MantineProvider } from "@mantine/core";
+import { Notifications } from "@mantine/notifications";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <MantineProvider withGlobalStyles withNormalizeCSS theme={{ colorScheme: "dark" }}>
+      <Notifications />
       <Component {...pageProps} />
     </MantineProvider>
   );
