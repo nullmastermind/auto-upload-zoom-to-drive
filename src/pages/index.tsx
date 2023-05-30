d1e25f374aa3e6f1c9139057c94e28890df7767b
bluvar <0x317374@gmail.com>
2023-05-30 17:23:02 +0700

    chore(checkpoint): 30/05/2023, 10:23:02 GMT+0000

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,9 @@
-import { Button, Card, Divider, TextInput, Title } from "@mantine/core";
+import { ActionIcon, Button, Card, Collapse, Divider, TextInput, Title } from "@mantine/core";
 import { useLocalStorage, useSetState } from "react-use";
 import Link from "next/link";
 import axios from "axios";
+import { IconSettings } from "@tabler/icons-react";
+import { useDisclosure } from "@mantine/hooks";
 
 export default function Home() {
   const [folderId, setFolderId] = useLocalStorage(":folderId", "");
@@ -11,14 +13,19 @@ export default function Home() {
   const [loadings, setLoadings] = useSetState({
     reload: false,
   });
+  const [opened, { toggle, close }] = useDisclosure(false);
 
   return (
-    <main className="m-auto max-w-xl">
-      <div>
+    <main className="m-auto max-w-xl py-10">
+      <div className={"flex flex-row gap-3 items-center"}>
+        <ActionIcon variant={"outline"} onClick={toggle}>
+          <IconSettings />
+        </ActionIcon>
         <Button
           loading={loadings.reload}
           variant="gradient"
           onClick={() => {
+            close();
             setLoadings({ reload: true });
             axios
               .post("/api/getVideos", {
@@ -37,56 +44,58 @@ export default function Home() {
           Reload Files
         </Button>
       </div>
-      <Card withBorder padding={"xs"} className={'my-3'}>
-        <Card.Section className={"px-6"}>
-          <Title order={3}>Settings</Title>
-        </Card.Section>
-        <div className="p-4 flex flex-col gap-2">
-          <TextInput
-            size={"xs"}
-            label="Folder ID"
-            value={folderId}
-            onChange={(e) => setFolderId(e.target.value)}
-            placeholder={"E.g: 1Gz5K3YtRNK6HZITIZ8rk-knuJpwD-uMv"}
-          />
-          <TextInput
-            size={"xs"}
-            label="Folder containing Zoom's video recordings"
-            value={zoomFolder}
-            onChange={(e) => setZoomFolder(e.target.value)}
-            placeholder={"E.g: Documents/Zoom"}
-          />
-          <TextInput
-            size={"xs"}
-            label="Access token"
-            value={accessToken}
-            onChange={(e) => setAccessToken(e.target.value)}
-          />
-          <TextInput
-            size={"xs"}
-            label="Refresh token"
-            value={refreshToken}
-            onChange={(e) => setRefreshToken(e.target.value)}
-          />
-          <div>
-            <Link href={"https://developers.google.com/oauthplayground"} target={"_blank"}>
-              https://developers.google.com/oauthplayground
-            </Link>
-            <div className={"flex flex-row gap-2"}>
-              <div className={"whitespace-nowrap opacity-60"}>Client ID:</div>
-              <TextInput
-                className={"w-full"}
-                size={"xs"}
-                value={"636038632941-rrlqtq7h3gp8l4ipu64d8pnunhpqrr8q.apps.googleusercontent.com"}
-              />
-            </div>
-            <div className={"flex flex-row gap-2 mt-1"}>
-              <div className={"whitespace-nowrap opacity-60"}>Client secret:</div>
-              <TextInput className={"w-full"} size={"xs"} value={"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"} />
+      <Collapse in={opened}>
+        <Card withBorder padding={"xs"} className={"my-3"}>
+          <Card.Section className={"px-6"}>
+            <Title order={3}>Settings</Title>
+          </Card.Section>
+          <div className="p-4 flex flex-col gap-2">
+            <TextInput
+              size={"xs"}
+              label="Folder ID"
+              value={folderId}
+              onChange={(e) => setFolderId(e.target.value)}
+              placeholder={"E.g: 1Gz5K3YtRNK6HZITIZ8rk-knuJpwD-uMv"}
+            />
+            <TextInput
+              size={"xs"}
+              label="Folder containing Zoom's video recordings"
+              value={zoomFolder}
+              onChange={(e) => setZoomFolder(e.target.value)}
+              placeholder={"E.g: Documents/Zoom"}
+            />
+            <TextInput
+              size={"xs"}
+              label="Access token"
+              value={accessToken}
+              onChange={(e) => setAccessToken(e.target.value)}
+            />
+            <TextInput
+              size={"xs"}
+              label="Refresh token"
+              value={refreshToken}
+              onChange={(e) => setRefreshToken(e.target.value)}
+            />
+            <div>
+              <Link href={"https://developers.google.com/oauthplayground"} target={"_blank"}>
+                https://developers.google.com/oauthplayground
+              </Link>
+              <div className={"flex flex-row gap-2"}>
+                <div className={"whitespace-nowrap opacity-60"}>Client ID:</div>
+                <TextInput
+                  className={"w-full"}
+                  size={"xs"}
+                  value={"636038632941-rrlqtq7h3gp8l4ipu64d8pnunhpqrr8q.apps.googleusercontent.com"}
+                />
+              </div>
+              <div className={"flex flex-row gap-2 mt-1"}>
+                <div className={"whitespace-nowrap opacity-60"}>Client secret:</div>
+                <TextInput className={"w-full"} size={"xs"} value={"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"} />
+              </div>
             </div>
           </div>
-        </div>
-      </Card>
+        </Card>
+      </Collapse>
     </main>
   );
 }
