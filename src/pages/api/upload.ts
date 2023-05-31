0cf9403aaac91fba20cdbc56dbcb34a1b7576dc2
bluvar <0x317374@gmail.com>
2023-05-31 13:14:23 +0700

    chore(checkpoint): 31/05/2023, 06:14:23 GMT+0000

--- src/pages/api/upload.ts
+++ src/pages/api/upload.ts
@@ -31,6 +31,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       requestBody: fileMetadata,
       media: media,
       fields: "id",
+      supportsAllDrives: true,
     });
 
     try {
